a221c843abf4101fd362a38949f40311081e4472
Thor Tveito <thortve@stud.ntnu.no>
2024-11-06 12:22:39 +0100

    add js

---
+++ Oving4/script.js
@@ -0,0 +1,37 @@
+/* Task  1.2 */
+console.log('Task  1.2')
+
+/* Task  1.3 */
+console.log('Task  1.3')
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+/* Task  1.4 */
+
+/* Task  1.5 */
+function changeDisplay () {
+
+}
+
+function changeVisibility () {
+
+}
+
+function reset () {
+
+}
+
+/* Task 1.6 */
+const technologies = [
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'Python',
+    'Java',
+    'AJAX',
+    'JSON',
+    'React',
+    'Angular',
+    'Bootstrap',
+    'Node.js'
+];
